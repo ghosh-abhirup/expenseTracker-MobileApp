d7d5be0df0f931979dca81ca419c463b46a7da63
abhirup_ghosh_29 <abhirup.ghosh@perch.club>
2023-10-06 14:50:50 +0530

    Expense list item

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,8 +1,9 @@
 import { StyleSheet } from "react-native";
 import { View, Text, FlatList } from "react-native";
+import ExpenseItem from "./ExpenseItem";
 
 const renderExpenseItem = (itemData) => {
-  return <Text>{itemData.item.description}</Text>;
+  return <ExpenseItem {...itemData.item} />;
 };
 
 export const ExpensesList = ({ data }) => {
